5dfcc4b6bfbb62eb22d3cd213f46e496e4ffa182
Void&Null <70048414+Void-n-Null@users.noreply.github.com>
2025-02-07 20:28:04 -0600

    Remove Gradle build configuration and project setup files from arch implimentation

--- fabric/src/main/java/net/voidnull/autobreed/fabric/AutoBreedFabric.java
+++
@@ -1,9 +0,0 @@
-package net.voidnull.autobreed.fabric;
-
-import net.fabricmc.api.ModInitializer;
-
-public class AutoBreedFabric implements ModInitializer
-{
-    @Override
-    public void onInitialize() {}
-}
