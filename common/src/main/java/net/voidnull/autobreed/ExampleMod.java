483eb84e978285ed7ca5511189fcd743025d7278
Void&Null <70048414+Void-n-Null@users.noreply.github.com>
2025-02-10 23:12:43 -0600

    Migrate project to Architectury multi-platform mod development setup

---
+++ common/src/main/java/net/voidnull/autobreed/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.voidnull.autobreed;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "autobreed";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
